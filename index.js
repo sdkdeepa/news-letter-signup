95c6c6c76a4af8528d72f61ef4193d679c45d5b0
Deepa Subramanian <deepa94086@gmail.com>
2025-01-17 03:13:19 -0800

    error handling to be fixed

--- index.js
+++ index.js
@@ -2,25 +2,25 @@ let container = document.querySelector('.container');
 let successToast = document.querySelector('.sucess__message');
 let emailContainer = document.querySelector('.input__email');
 let emailInput = document.querySelector('input[type="email"]');
-let submit = document.querySelector('input[type="submit"]');
+let submitButton = document.querySelector('input[type="submit"]');
 let error = document.querySelector('.invalid-email');
 let emailService = document.querySelector('.email__service');
 
 let userEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.(com|org)$/ ;
 
-submit.addEventListener('click', (e)=> {
+submitButton.addEventListener('click',(e)=>{
     e.preventDefault();
-    if(emailInput.value === "" | (!userEmail.test(userEmail.value))) {
+    if(emailInput.value === "" || (!emailRegex.test(emailInput.value))){
         error.classList.remove('inactive');
         emailContainer.classList.add('error');
-        userEmail.classList.add('invalid-email');
-        userEmail.style.color = "red"
-    }else {
+        emailInput.classList.add('invalid-email')
+        emailInput.style.color = "red";
+    } else {
         document.querySelector('.image').style.display ='none';
         document.querySelector('.copy__text').style.display ='none';
         successToast.classList.remove('inactive');
         emailService.innerText = emailInput.value;
-        if(window.innerWidth >=750) {
+        if(window.innerWidth >=750){
             container.classList.add('success__message');
         }
     }
@@ -29,5 +29,5 @@ submit.addEventListener('click', (e)=> {
 emailInput.addEventListener('focus' , () => {
     emailInput.style.color = "hsl(234, 29%, 20%)";
     emailContainer.classList.remove('error');
-    emailInput.classList.remove('invalid-email');
+    emailInput.classList.remove('error-mail');
 })
